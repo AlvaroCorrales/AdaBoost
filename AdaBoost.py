6fd7bc4fb0a80d0b22443beb527d881105258f7a
Álvaro Corrales <54533013+AlvaroCorrales@users.noreply.github.com>
2021-04-14 12:30:07 +0100

    Included visual inspection of model

--- AdaBoost.py
+++ AdaBoost.py
@@ -8,28 +8,30 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
 # Helper functions
-def compute_error(w_i, y, y_pred):
+def compute_error(y, y_pred, w_i):
     '''
-    Calculate the error rate of a weak classifier m. Parameters:
-    w_i: individual weights for each observation
+    Calculate the error rate of a weak classifier m. Arguments:
     y: actual target value
     y_pred: predicted value by weak classifier
+    w_i: individual weights for each observation
+
     
     Note that all arrays should be the same length
     '''
+
     return (sum(w_i * (np.not_equal(y, y_pred)).astype(int)))/sum(w_i)
 
 def compute_alpha(error):
     '''
     Calculate the weight of a weak classifier m in the majority vote of the final classifier. This is called
-    alpha in chapter 10.1 of The Elements of Statistical Learning. Parameters:
+    alpha in chapter 10.1 of The Elements of Statistical Learning. Arguments:
     error: error rate from weak classifier m
     '''
     return np.log((1 - error) / error)
 
 def update_weights(w_i, alpha, y, y_pred):
     ''' 
-    Update individual weights w_i after a boosting iteration. Parameters:
+    Update individual weights w_i after a boosting iteration. Arguments:
     w_i: individual weights for each observation
     y: actual target value
     y_pred: predicted value by weak classifier  
@@ -45,19 +47,24 @@ class AdaBoost:
         self.alphas = []
         self.G_M = []
         self.M = None
+        self.training_errors = []
+        self.prediction_errors = []
 
     def fit(self, X, y, M = 100):
         '''
-        Fit model. Parameters:
+        Fit model. Arguments:
         X: independent variables
         y: target variable
         M: number of boosting rounds. Default is 100
         '''
-
+        
+        # Clear before calling
+        self.alphas = [] 
+        self.training_errors = []
         self.M = M
 
         # Iterate over M weak classifiers
-        for m in range(0, M + 1):
+        for m in range(0, M):
             
             # Set weights for current boosting iteration
             if m == 0:
@@ -74,7 +81,8 @@ class AdaBoost:
             self.G_M.append(G_m) # Save to list of weak classifiers
 
             # (b) Compute error
-            error_m = compute_error(w_i, y, y_pred)
+            error_m = compute_error(y, y_pred, w_i)
+            self.training_errors.append(error_m)
             # print(error_m)
 
             # (c) Compute alpha
@@ -87,7 +95,7 @@ class AdaBoost:
 
     def predict(self, X):
         '''
-        Predict using fitted model. Parameters:
+        Predict using fitted model. Arguments:
         X: independent variables
         '''
 
@@ -104,4 +112,17 @@ class AdaBoost:
 
         return y_pred
       
-
+    def error_rates(self, X, y):
+        '''
+        Get the error rates of each weak classifier. Arguments:
+        X: independent variables
+        y: target variables associated to X
+        '''
+        
+        self.prediction_errors = [] # Clear before calling
+        
+        # Predict class label for each weak classifier
+        for m in range(self.M):
+            y_pred_m = self.G_M[m].predict(X)          
+            error_m = compute_error(y = y, y_pred = y_pred_m, w_i = np.ones(len(y)))
+            self.prediction_errors.append(error_m)
